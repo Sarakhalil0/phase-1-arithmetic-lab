1fae2f2f47e84bbddbe5195afc32d4d7ca17754e
sara khalil <sarakhalil3377@gmail.com>
2022-07-31 22:09:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+// Write your code here
+//first
+const num1 = 2;
+const num2 = 31;
+const multiply = (num1 * num2);
+console.log(multiply);
+
+//second
+const random = Math.floor(Math.random() * 10) + 1;
+Number.isInteger(random);
+console.log(random);
+
+//third
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+//forth
+const max = Math.max(1, 4, 13, 11, 20);
+console.log(max);
